78db8d43790c4be9e5a20dbfe27f303ab944c5a4
ponces <saul.ponce@sv.muhlbauer.com>
2021-05-30 22:49:23 -0600

    Configure to built to war

--- src/main/java/com/equifax/dev/ChallengeApplication.java
+++ src/main/java/com/equifax/dev/ChallengeApplication.java
@@ -2,14 +2,21 @@ package com.equifax.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class ChallengeApplication {
-
+public class ChallengeApplication extends SpringBootServletInitializer{
+	 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(ChallengeApplication.class, args);
 		
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ChallengeApplication.class);
+    }
 }
