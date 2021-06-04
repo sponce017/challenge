fa365653b39b09592db965de52182e6bb23e32cf
ponces <saul.ponce@sv.muhlbauer.com>
2021-06-04 15:38:56 -0600

    Fix Classpath queries

--- src/main/java/com/equifax/dev/utils/Constants.java
+++ src/main/java/com/equifax/dev/utils/Constants.java
@@ -4,6 +4,6 @@ package com.equifax.dev.utils;
 public class Constants {
 
     public static final String DEFAULT_WEB_ENCODING = "ISO-8859-1";
-    public static final String DAO_QUERIES_LOCATION = "queries/*";
+    public static final String DAO_QUERIES_LOCATION = "classpath:/queries/*";
 
 }
